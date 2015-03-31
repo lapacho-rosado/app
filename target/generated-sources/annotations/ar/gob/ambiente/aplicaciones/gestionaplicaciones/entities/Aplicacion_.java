7c94229b672537a6eaa6093ef8b079a952ef6a58
Ruben Incostante <rincostante@ambiente.gob.ar>
2015-03-31 17:16:04 -0300

    Primer commit

---
+++ target/generated-sources/annotations/ar/gob/ambiente/aplicaciones/gestionaplicaciones/entities/Aplicacion_.java
@@ -0,0 +1,20 @@
+package ar.gob.ambiente.aplicaciones.gestionaplicaciones.entities;
+
+import ar.gob.ambiente.aplicaciones.gestionaplicaciones.entities.Usuario;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2015-03-25T18:48:37")
+@StaticMetamodel(Aplicacion.class)
+public class Aplicacion_ { 
+
+    public static volatile SingularAttribute<Aplicacion, Long> id;
+    public static volatile SingularAttribute<Aplicacion, String> nombre;
+    public static volatile SingularAttribute<Aplicacion, String> areaReferente;
+    public static volatile SingularAttribute<Aplicacion, String> descripcion;
+    public static volatile SingularAttribute<Aplicacion, String> url;
+    public static volatile ListAttribute<Aplicacion, Usuario> usuarios;
+
+}
