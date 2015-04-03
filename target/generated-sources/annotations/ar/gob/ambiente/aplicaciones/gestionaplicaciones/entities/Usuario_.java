4b85fa5f1bfdeb3d17857e02ecb320ec7e5ba6e2
Ruben Incostante <rincostante@ambiente.gob.ar>
2015-04-03 18:10:11 -0300

    primer commit local portatil

--- target/generated-sources/annotations/ar/gob/ambiente/aplicaciones/gestionaplicaciones/entities/Usuario_.java
+++ target/generated-sources/annotations/ar/gob/ambiente/aplicaciones/gestionaplicaciones/entities/Usuario_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2015-03-25T18:48:37")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2015-04-03T18:09:39")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
