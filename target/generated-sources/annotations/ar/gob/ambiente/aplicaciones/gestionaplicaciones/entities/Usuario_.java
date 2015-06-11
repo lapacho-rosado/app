e3de036e60d3b76e466686478595cb33c91acad9
Ruben Incostante <rincostante@ambiente.gob.ar>
2015-06-11 14:45:48 -0300

    Se actualizó todo para subir a testing

--- target/generated-sources/annotations/ar/gob/ambiente/aplicaciones/gestionaplicaciones/entities/Usuario_.java
+++ target/generated-sources/annotations/ar/gob/ambiente/aplicaciones/gestionaplicaciones/entities/Usuario_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2015-05-22T15:33:59")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2015-06-11T12:27:15")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
