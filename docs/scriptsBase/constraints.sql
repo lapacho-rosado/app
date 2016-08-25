c9f6d927ac65a5be7c766a81564685248ddf4613
Ruben Incostante <rincostante@ambiente.gob.ar>
2016-08-25 16:03:21 -0300

    Se actualizó la documentación

---
+++ docs/scriptsBase/constraints.sql
@@ -0,0 +1,38 @@
+﻿/*
+ CONSTRAINTS usuario
+ */
+
+
+ALTER TABLE ONLY usuario
+    ADD CONSTRAINT fk_usuario_adminentidad_id FOREIGN KEY (admin_id) REFERENCES adminentidad(id);
+
+
+ALTER TABLE ONLY usuario
+    ADD CONSTRAINT fk_usuario_rol_id FOREIGN KEY (rol_id) REFERENCES rol(id);
+
+
+/*
+ CONSTRAINTS rol
+ */
+
+ALTER TABLE ONLY rol
+    ADD CONSTRAINT fk_rol_adminentidad_id FOREIGN KEY (admin_id) REFERENCES adminentidad(id);
+
+
+/*
+ CONSTRAINTS adminentidad
+ */
+
+
+ALTER TABLE ONLY adminentidad
+    ADD CONSTRAINT fk_adminentidad_usalta_id FOREIGN KEY (usalta_id) REFERENCES usuario(id);
+
+
+ALTER TABLE ONLY adminentidad
+    ADD CONSTRAINT fk_adminentidad_usbaja_id FOREIGN KEY (usbaja_id) REFERENCES usuario(id);
+
+
+ALTER TABLE ONLY adminentidad
+    ADD CONSTRAINT fk_adminentidad_usmodif_id FOREIGN KEY (usmodif_id) REFERENCES usuario(id);
+
+
---
+++ docs/scriptsBase/rol.sql
@@ -0,0 +1,104 @@
+﻿--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.3.4
+-- Dumped by pg_dump version 9.3.4
+-- Started on 2016-08-24 23:44:08
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- TOC entry 203 (class 1259 OID 256500)
+-- Name: rol; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE rol (
+    id integer NOT NULL,
+    nombre character varying(50) NOT NULL,
+    admin_id bigint
+);
+
+
+ALTER TABLE public.rol OWNER TO postgres;
+
+--
+-- TOC entry 204 (class 1259 OID 256503)
+-- Name: rol_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE rol_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.rol_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 2095 (class 0 OID 0)
+-- Dependencies: 204
+-- Name: rol_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE rol_id_seq OWNED BY rol.id;
+
+
+--
+-- TOC entry 1977 (class 2604 OID 256532)
+-- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY rol ALTER COLUMN id SET DEFAULT nextval('rol_id_seq'::regclass);
+
+
+--
+-- TOC entry 2089 (class 0 OID 256500)
+-- Dependencies: 203
+-- Data for Name: rol; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO rol VALUES (1, 'Administrador', 2);
+INSERT INTO rol VALUES (2, 'Seguridad', 3);
+
+
+--
+-- TOC entry 2096 (class 0 OID 0)
+-- Dependencies: 204
+-- Name: rol_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('rol_id_seq', 2, true);
+
+
+--
+-- TOC entry 1979 (class 2606 OID 256569)
+-- Name: rol_nombre_key; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY rol
+    ADD CONSTRAINT rol_nombre_key UNIQUE (nombre);
+
+
+--
+-- TOC entry 1981 (class 2606 OID 256571)
+-- Name: rol_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY rol
+    ADD CONSTRAINT rol_pkey PRIMARY KEY (id);
+
+
