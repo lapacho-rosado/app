f63f5873868a8e87b8a84d11e64d86e2a230751e
Ruben Incostante <rincostante@ambiente.gob.ar>
2019-03-12 16:36:28 -0300

    Se actualizó el proceso de validación de usuarios y el README

--- docs/DB/actUsuario.sql
+++
@@ -1,8 +0,0 @@
-﻿-- Agegado de la nueva columna en para guardar el rol
-
-ALTER TABLE usuario ADD COLUMN rol_id bigint;
-
-
--- Eliminar el campo "persona"
-
-ALTER TABLE usuario DROP COLUMN persona;
